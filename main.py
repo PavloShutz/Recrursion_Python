427ea160b75bba91a76b4bd81c377d4a8bcf9595
PavloShutz <pavlo050706@gmail.com>
2022-05-24 20:32:32 +0300

    Changes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def is_palindrome(string: str) -> bool:
     """Return True if the given string is a palindrome"""
+    if not isinstance(string, str):
+        return False
     string = ''.join(elem for elem in string.lower().split())
     if len(string) < 2:
         return True
--- recursion_lists.py
+++ recursion_lists.py
@@ -1,12 +1,7 @@
 from typing import Union
 
 
-def get_sum_from_lists(lists: list) -> Union[int, float]:
-    """Returns sum of all elements from given list"""
-    nums = []
-    for number in lists:
-        if not isinstance(number, list):
-            nums.append(number)
-        else:
-            nums.append(get_sum_from_lists(number))
-    return sum(nums)
+def refactored_sum_of_lists(lists: list) -> Union[int, float]:
+    return sum([number if not isinstance(number, list)
+                else refactored_sum_of_lists(number)
+                for number in lists])
